b3fe98e5bbd8898ab4e0ef4ff72cbb1cfb6ea01a
ahorriblename <124475233+ahorriblename@users.noreply.github.com>
2025-05-19 12:29:38 -0700

    Add constructors to Enemy.java and Player.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,2 +1,10 @@
 public class Enemy extends Entity{
+
+    Enemy() {
+        super();
+    }
+
+    Enemy(int health, int speed, int strength, int stamina, int armor) {
+        super(health, speed, strength, stamina, armor);
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,11 @@
 public class Player extends Entity{
+
+
+    Player() {
+        super();
+    }
+
+    Player(int health, int speed, int strength, int stamina, int armor) {
+        super(health, speed, strength, stamina, armor);
+    }
 }
