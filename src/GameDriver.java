64a8c12bdcc00371ddbf87d49a8ebf54d609e2f9
ahorriblename <124475233+ahorriblename@users.noreply.github.com>
2025-05-24 13:20:45 -0700

    Create getPlayerName method in GameDriver

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GameDriver {
     public static void main(String[] args) {
         Goblin defaultEnemy = new Goblin();
@@ -6,13 +8,22 @@ public class GameDriver {
         defaultEnemy.setTarget(player1);
         player1.setTarget(defaultEnemy);
 
-        player1.setName("Player 1");
+        player1.setName(getPlayerName());
+        System.out.println("Hello " + player1.getName() + "!");
 
+        /*
         player1.crush();
 
         defaultEnemy.slash();
 
         System.out.println(defaultEnemy);
         System.out.println(player1);
+         */
+    }
+
+    static String getPlayerName() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        return input.nextLine();
     }
 }
